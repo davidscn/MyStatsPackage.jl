6c68479a0a611be0b9dd84f7fb2891569c3bbffd
David Schneider <david.schneider@ipvs.uni-stuttgart.de>
2023-10-10 15:10:37 +0200

    Apply formatting

--- src/statistic_functions.jl
+++ src/statistic_functions.jl
@@ -1,15 +1,15 @@
 function rse_sum(a)
-sum = 0;
-    for i = 1: length(a)
+    sum = 0
+    for i = 1:length(a)
         sum += a[i]
     end
- return sum;
+    return sum
 end
 
 
 function rse_mean(a)
-    sum = rse_sum(a);
-    return sum/length(a);
+    sum = rse_sum(a)
+    return sum / length(a)
 end
 
 function rse_std(a)
@@ -17,10 +17,10 @@ function rse_std(a)
     dev = sum.(a .- mean)
     dev = dev .* dev
     dev = sum(dev)
-    dev = sqrt(dev/(length(a)-1))
+    dev = sqrt(dev / (length(a) - 1))
     return dev
 end
 
 function rse_tstat(a; sigma = "val")
-    return rse_mean(a)/(rse_std(a)/sqrt(length(a)))
+    return rse_mean(a) / (rse_std(a) / sqrt(length(a)))
 end
